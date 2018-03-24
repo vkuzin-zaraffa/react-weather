6dcdaed6a95d16334c8a0ab7471dd0facce1a125
Valerii <vkuzin@zaraffasoft.com>
2018-03-24 16:37:42 +0200

    5.54 Added method onFormSubmit

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -17,9 +17,13 @@ export default class SearchBar extends Component {
     this.setState({term: event.target.value})
   }
 
+  onFormSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
     return (
-      <form className="input-group">
+      <form onSubmit={this.onFormSubmit} className="input-group">
         <input
           placeholder="Get a five-day forecast in your favorite city"
           className="form-control"
