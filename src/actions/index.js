eefd0fb1139d59faa9f4a0bbd5618e4c148f298a
Valerii <vkuzin@zaraffasoft.com>
2018-03-24 20:10:54 +0200

    5.57 Added axios request

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,17 @@
+import axios from 'axios'
+
 const API_KEY = '98c7d1bcabb7269788971a21325cc4ab';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+
+  const url = `${ROOT_URL}&q=${city}`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
